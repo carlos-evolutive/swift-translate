45945ce9b0082c8d68207340593237ef37e26052
Carlos <carlos+git@evolutive.app>
2025-03-24 15:04:48 +0100

    Updated stale state, setting high reasoning effort for reasoning models.

--- Sources/SwiftStringCatalog/Bootstrap/StringCatalog.swift
+++ Sources/SwiftStringCatalog/Bootstrap/StringCatalog.swift
@@ -35,7 +35,7 @@ public final class StringCatalog {
     public init(url: URL, configureWith targetLanguages: Set<Language>? = nil) throws {
         let data = try Data(contentsOf: url)
         let decoder = JSONDecoder()
-        let catalog = try decoder.decode(_StringCatalog.self, from: data)
+        var catalog: _StringCatalog!
         if catalog.version != version {
             throw Error.catalogVersionNotSupported(catalog.version)
         }
@@ -78,7 +78,7 @@ public final class StringCatalog {
         for (key, entry) in catalog.strings {
             let sourceLanguageStrings = try sourceLanguageStrings(in: entry, for: key)
             self.sourceLanguageStrings[key] = sourceLanguageStrings
-            
+            print("localizableStrings for \(key). entry: \(entry), sourceLanguageStrings: \(sourceLanguageStrings)")
             let localizableStrings = try localizableStrings(in: entry, for: key, referencing: sourceLanguageStrings)
             localizableStringsCount += localizableStrings.count
             localizableStringGroups[key] = LocalizableStringGroup(
